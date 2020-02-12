196beea6c0e715791bb2a70e8e31f453c1833d8e
Theodore Sterne <sterne_theodore@network.lilly.com>
2020-02-12 14:07:17 -0500

    Switch to JavaScript

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-  </div>
+const element = React.createElement(
+  "div",
+  { id: "foo" },
+  React.createElement("a", null),
+  React.createElement("b", null)
 );
 const container = document.getElementById("root");
 ReactDOM.render(element, container);
