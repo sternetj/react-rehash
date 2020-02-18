543e30b2174bceeefa362368b0f16e8068856140
Theodore Sterne <sterne_theodore@network.lilly.com>
2020-02-18 14:27:59 -0500

    Add work loop

--- src/rehash.js
+++ src/rehash.js
@@ -14,8 +14,8 @@ function createTextElement(text) {
   return createElement("TEXT_ELEMENT", { nodeValue: text });
 }
 
-function render(element, container) {
-  const { type, props } = element;
+function createDom(fiber) {
+  const { type, props } = fiber;
   const { children, ...props } = props;
 
   const dom =
@@ -24,9 +24,79 @@ function render(element, container) {
       : document.createElement(type);
 
   Object.entries(props).forEach(([attr, value]) => (dom[attr] = value));
-  children.forEach(child => render(child, dom));
 
-  container.appendChild(dom);
+  return dom;
+}
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  };
+}
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  while (nextUnitOfWork && !deadline.didTimeout) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+  }
+
+  window.requestIdleCallback(workLoop);
+}
+
+window.requestIdleCallback(workLoop);
+
+/**
+ * @param {Fiber} fiber
+ * @typedef {{
+ *   type?: string
+ *   dom?: HTMLElement,
+ *   parent?: Fiber,
+ *   child?: Fiber,
+ *   sibling?: Fiber,
+ *   props: {
+ *     children: Fiber[]
+ *   }
+ * }} Fiber
+ */
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
+  let prevSibling = null;
+  elements.forEach((element, index) => {
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+  });
+
+  if (fiber.child) return fiber.child;
+
+  const nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling;
+    nextFiber = nextFiber.parent;
+  }
 }
 
 module.exports = {
